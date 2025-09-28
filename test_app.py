f82848024c634cfe9c43b6d8d4144b287d775cc3
perseedev <perseedev@hotmail.com>
2025-09-28 09:52:16 +0800

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add_positive(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative(self):
+        self.assertEqual(add(-1, -2), -3)
+
+if __name__ == '__main__':
+    unittest.main()
